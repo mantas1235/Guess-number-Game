69bf702069051fe055698e8bc7fd8ce71fc1620b
Mantas Santockis <mantas.san@gmail.com>
2022-12-13 15:06:40 +0200

    reaching value of input

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+console.log(document.querySelector(".message").textContent);
+
+
+document.querySelector('.message').textContent = "Correct Number 🎉🎉🎉"
+
+document.querySelector('.number').textContent = 13
+document.querySelector('.score').textContent = 10
+
+document.querySelector('.guess').value
+document.querySelector('.guess').value = 23
